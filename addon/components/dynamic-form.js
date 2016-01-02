10c749ec2b96b4552f416f21de55b46dd1cd3dd5
Todd Jordan <todd.jordan@inin.com>
2016-01-01 22:10:33 -0500

    initial support for filter rules
    filter rule review feedback

--- addon/components/dynamic-form.js
+++ addon/components/dynamic-form.js
@@ -20,7 +20,30 @@ const DynamicForm = Ember.Component.extend({
   },
 
   didReceiveAttrs() {
-    const schema = this.attrs.schema.value;
+    let schemaObj = this._initSchema(this.get('schema'));
+    const filteredSchema = this._processFilters(schemaObj);
+    const mappedSchema = this._replaceKeywordsWithFunctions(filteredSchema);
+    this.set('renderSchema', mappedSchema);
+  },
+
+  _processFilters(schemaObj) {
+    if (!(schemaObj && schemaObj.options && schemaObj.options.fields)) {
+      return schemaObj;
+    }
+    const optionFields = schemaObj.options.fields;
+    const newSchema = _.reduce(optionFields, (result, val, key) => {
+      if(val['filter-rules']) {
+        val['filter-rules'].forEach((element) => {
+          const filterRule = this.container.lookup(`${element}:forms.filter-rules`);
+          filterRule.filter(key, result);
+        });
+      }
+      return result;
+    }, _.clone(schemaObj, true));
+    return newSchema;
+  },
+
+  _initSchema(schema) {
     let schemaObj;
     if (typeof schema === 'string') {
       schemaObj = JSON.parse(schema);
@@ -31,21 +54,25 @@ const DynamicForm = Ember.Component.extend({
       const postRenderFn = this.attrs.postRender.value;
       schemaObj["postRender"] = postRenderFn;
     }
-    const myfun = (object, value, key) => {
+    return schemaObj;
+  },
+
+  _replaceKeywordsWithFunctions(schemaObj) {
+    const container = this.container;
+    const replaceWithFunction = function (object, value, key) {
       if (TYPE_MAP.hasOwnProperty(key) && typeof value === 'string') {
         const type = TYPE_MAP[key];
-        const typeObj = this.container.lookup(`${value}:${type.namespace}`);
+        const typeObj = container.lookup(`${value}:${type.namespace}`);
         if (typeObj) {
           object[key] = typeObj[type.functionName];
         } // else fail with a message that the given type couldn't be found
       } else if (typeof value === 'object') {
-        object[key] = _.transform(value, myfun);
+        object[key] = _.transform(value, replaceWithFunction);
       } else {
         object[key] = value;
       }
     };
-    const mappedSchema = _.transform(schemaObj, myfun);
-    this.set('renderSchema', mappedSchema);
+    return  _.transform(schemaObj, replaceWithFunction);
   }
 });
 
---
+++ tests/integration/components/filter-rules-test.js
@@ -0,0 +1,53 @@
+import Ember from 'ember';
+import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from 'ember-qunit';
+
+const basicObject = {
+  "schema": {
+    "title": "What do you think of Alpaca?",
+    "type": "object",
+    "properties": {
+      "name": {
+        "type": "string",
+        "title": "Name"
+      },
+      "ranking": {
+        "type": "string",
+        "title": "Ranking",
+        "enum": ['excellent', 'not too shabby', 'alpaca built my hotrod']
+      }
+    }
+  },
+  "options": {
+    "fields": {
+      "name": {
+        "filter-rules": ["admin-only"]
+      }
+    }
+  }
+};
+
+const filterRule = Ember.Object.extend({
+  filter(key, schemaObj) {
+    const fieldOption = schemaObj.options.fields[key];
+    fieldOption.disabled = true;
+  }
+});
+
+
+moduleForComponent('dynamic-form', 'dynamic-form:filter rules', {
+  integration: true,
+  beforeEach() {
+    this.register('admin-only:forms.filter-rules', filterRule);
+  }
+});
+
+test('should disable a field based a users role', function (assert) {
+  const done = assert.async();
+  basicObject['postRender'] = () => {
+    assert.ok(this.$('.alpaca-field-text input').prop('disabled'), 'field is disabled');
+    done();
+  };
+  this.set('basicObject', basicObject);
+  this.render(hbs`{{dynamic-form schema=basicObject}}`);
+});
