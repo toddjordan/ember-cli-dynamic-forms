06eb4e349dcf92e6ce9c3c70649d15a2fcf57bff
Todd Jordan <todd.jordan@inin.com>
2016-09-12 16:16:51 -0400

    forgot to update index js

--- blueprints/ember-cli-dynamic-forms/index.js
+++ blueprints/ember-cli-dynamic-forms/index.js
@@ -6,7 +6,7 @@ module.exports = {
   afterInstall: function () {
     return this.addBowerPackagesToProject([
       { name: 'jquery', target: '>=1.11.3' },
-      { name: 'alpaca', target: '1.5.14' },
+      { name: 'alpaca', target: 'ade59b36bebb0f2de04aa34a05c44e23281ea17a' },
       { name: 'handlebars', target: '3.0.3' },
       { name: 'bootstrap', target: '3.3.2' },
       { name: 'lodash', target: '~4.6.1' }
