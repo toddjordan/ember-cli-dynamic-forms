4762700684a015bb93c4e5fe1221342b158323ba
Todd Jordan <todd.jordan@inin.com>
2016-10-04 12:05:00 -0400

    upgrade jquery

--- blueprints/ember-cli-dynamic-forms/index.js
+++ blueprints/ember-cli-dynamic-forms/index.js
@@ -5,8 +5,8 @@ module.exports = {
 
   afterInstall: function () {
     return this.addBowerPackagesToProject([
-      { name: 'jquery', target: '>=1.11.3' },
-      { name: 'alpaca', target: 'ade59b36bebb0f2de04aa34a05c44e23281ea17a' },
+      { name: 'jquery', target: '~2.1.0' },
+      { name: 'alpaca', target: '~1.5.22' },
       { name: 'handlebars', target: '3.0.3' },
       { name: 'bootstrap', target: '3.3.2' },
       { name: 'lodash', target: '~4.6.1' }
